57c97d9d4e69c73cf68b0b0bd93aafc0e90040d5
Hady Ibrahim <hadyib007@gmail.com>
2024-01-14 00:04:50 -0500

    update addr

--- FaceDetection/raspberrypi/doorlock/client.py
+++ FaceDetection/raspberrypi/doorlock/client.py
@@ -3,7 +3,7 @@ import servo_pb2_grpc
 import servo_pb2
 import time
 def run():
-  with grpc.insecure_channel('192.168.0.108:8081') as channel:
+  with grpc.insecure_channel('192.168.40.51:8081') as channel:
     stub = servo_pb2_grpc.DoorLockStub(channel)
     response = stub.Unlock(servo_pb2.UnlockRequest())
     print("Unlocked client received: " + str(response.success))
