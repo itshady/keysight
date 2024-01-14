7f36ebc6618f563ef7894f49bd8da8ee2b046c5e
Hady Ibrahim <hadyib007@gmail.com>
2024-01-14 05:06:29 -0500

    update servo

--- FaceDetection/raspberrypi/doorlock/servo_server.py
+++ FaceDetection/raspberrypi/doorlock/servo_server.py
@@ -5,6 +5,14 @@ from time import sleep   # Imports sleep (aka wait or pause) into the program
 
 
 class DoorLock(servo_pb2_grpc.DoorLock):
+  def __init__(self) -> None:
+    super().__init__()
+    GPIO.setmode(GPIO.BOARD) # Sets the pin numbering system to use the physical layout
+
+    # Set up pin 11 for PWM
+    GPIO.setup(11,GPIO.OUT)  # Sets up pin 11 to an output (instead of an input)
+    self.p = GPIO.PWM(11, 50)     # Sets up pin 11 as a PWM pin
+
   def Unlock(self, request, context):
     self.runServo(goLeft=True)
     print("unlock requested")
@@ -17,25 +25,19 @@ class DoorLock(servo_pb2_grpc.DoorLock):
     return servo_pb2.LockResponse(success=1)
 
   def runServo(self, goLeft=True):
-    GPIO.setmode(GPIO.BOARD) # Sets the pin numbering system to use the physical layout
-
-    # Set up pin 11 for PWM
-    GPIO.setup(11,GPIO.OUT)  # Sets up pin 11 to an output (instead of an input)
-    p = GPIO.PWM(11, 50)     # Sets up pin 11 as a PWM pin
-    p.start(0)               # Starts running PWM on the pin and sets it to 0
-
     # THREAD THIS
     # if goLeft:
     #   p.ChangeDutyCycle(3)     # Changes the pulse width to 3 (so moves the servo)
     # else:
     #   p.ChangeDutyCycle(12)    # Changes the pulse width to 12 (so moves the servo)
     # sleep(1)
+    self.p.start(0)               # Starts running PWM on the pin and sets it to 0
 
-    p.ChangeDutyCycle(10.5)
+    self.p.ChangeDutyCycle(10.5)
     sleep(3)
-    p.ChangeDutyCycle(3)
+    self.p.ChangeDutyCycle(3)
     sleep(1)
 
     # Clean up everything
-    p.stop()                 # At the end of the program, stop the PWM
+    self.p.stop()                 # At the end of the program, stop the PWM
     GPIO.cleanup()           # Resets the GPIO pins back to defaults
