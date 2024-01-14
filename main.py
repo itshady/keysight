9061fbd820f6e86e1a0fa94077279f2ec6bc3ee9
Hady Ibrahim <hadyib007@gmail.com>
2024-01-14 01:46:37 -0500

    add 1 more script

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ def run_script(script_name):
 if __name__ == "__main__":
   script1_thread = threading.Thread(target=run_script, args=("FaceDetection/VoiceRecognition.py",))
   script2_thread = threading.Thread(target=run_script, args=("FaceDetection/main.py",))
+  script3_thread = threading.Thread(target=run_script, args=("FaceDetection/interface.py",))
 
   script1_thread.start()
   script2_thread.start()
+  script3_thread.start()
 
   script1_thread.join()
   script2_thread.join()
+  script3_thread.join()
 
   print("Both scripts have finished executing.")
